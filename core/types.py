b823fef112bce35d8c9582b99a1fc36476459571
Guilherme Lazari <guilherme.lazari@wildlifestudios.com>
2021-03-20 13:43:22 -0300

    Add not implemented errors to interface
    This will make it easier to identify a class that does not properly
    implement this interface.

--- core/types.py
+++ core/types.py
@@ -20,12 +20,14 @@ class EventStream:
 
         return a success bool
         """
+        raise NotImplementedError("send")
 
     def list_events(self, client: str, event: str) -> List[dict]:
         """ Returns current contents of stream topic
 
         mostly used for testing
         """
+        raise NotImplementedError("list_events")
 
 class SchemaStorage:
     """ Interface for interacting with the SchemaStorage """
@@ -35,10 +37,13 @@ class SchemaStorage:
         returns true on success
         returns false if another event is registered with this
         """
+        raise NotImplementedError("register_event")
 
     def get_event(self, event_name: str, client: str) -> dict:
         """ Returns the stored schema for this name, None if not present"""
+        raise NotImplementedError("get_event")
 
     def list_events(self) -> List[tuple]:
         """ Returns a list of all events as a tuple (client, event_name, schema)
         """
+        raise NotImplementedError("list_events")
